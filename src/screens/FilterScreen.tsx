ad1cd95c68465ab00b399db4e4bc9c658df76d68
ThanhTu-Phan <pttu2902@gmail.com>
2023-05-05 13:47:11 +0700

    pull request

---
+++ src/screens/FilterScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const FilterScreen = () => {
+  return (
+    <View>
+      <Text>FilterScreen</Text>
+    </View>
+  )
+}
+
+export default FilterScreen
