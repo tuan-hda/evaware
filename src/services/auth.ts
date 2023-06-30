c3302d0b3318faaf5260e77903b23ea0f36a77f2
ThanhTu-Phan <pttu2902@gmail.com>
2023-06-30 18:12:59 +0700

    .

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,11 +1,14 @@
 import { UserProps } from '~/types/user.type'
 import { service } from './base'
 import { callAxios } from '~/utils/callAxios'
+import axios from 'axios'
 
 const path = '/auth'
 
 export const loginService = async (email: string, password: string) => {
-  return await callAxios<UserProps>(service.post(path + '/login', { email, password }))
+  // return await callAxios<UserProps>(service.post(path + '/login', { email, password }))
+  return await callAxios<UserProps>(axios.post('192.168.11.149:8000/api/auth/login', { email, password }))
+  
 }
 
 export const signUpService = async (email: string, password: string) => {
